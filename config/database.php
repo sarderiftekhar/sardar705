de162860029d28b3159c2ed7395aec36731158d6
Sarder <sarder2008@gmail.com>
2020-07-29 16:04:47 +0100

    laravel backup

--- config/database.php
+++ config/database.php
@@ -45,6 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'dump' => ['dump_binary_path' => 'C:\xampp\mysql\bin'], 
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
